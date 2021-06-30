80cad9f8690e2c4600ac5668ef6436f9428fd56e
Jomar Lipon <jomarlipon2012@yahoo.com>
2021-06-30 18:43:26 +0800

    updated inv_invoice_page
    Added the status filter menu..

--- invoice_dashboard/inc/inv_invoice_page.php
+++ invoice_dashboard/inc/inv_invoice_page.php
@@ -1,5 +1,25 @@
 <div class="invoice-wrap">
-    <h1>
-        Invoices
-    </h1>
+    <div class="title-wrap">
+        <h1>
+            Invoices
+        </h1>
+    </div>
+    <div class="filter-wrap">
+        <div cass="container">
+            <div clss="row">
+                <div class="col-lg-6 col-md-6">
+                    <div class="status-filter">
+                        <ul class="nav nav-tabs">
+                            <li class="nav-item"><a href="#" class="all active">All</a></li>
+                            <li class="nav-item"><a href="#" class="ongoing">Ongoing</a></li>
+                            <li class="nav-item"><a href="#" class="verified">Verified</a></li>
+                            <li class="nav-item"><a href="#" class="pending">Pending</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6">
+                </div>
+            </div> 
+        </div>
+    </div>
 </div>
